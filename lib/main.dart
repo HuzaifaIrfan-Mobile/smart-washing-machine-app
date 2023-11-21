1f79abff2a5089ef0e1394c4feeb58256c35d19d
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2023-11-21 23:46:01 +0500

    wakelock

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'washing_machine/washing_machine.dart';
 
+import 'package:flutter/foundation.dart';
+import 'package:wakelock/wakelock.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (defaultTargetPlatform == TargetPlatform.android) {
+// YOUR CODE
+    Wakelock.enable();
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // @override
+  // void dispose() {
+  //   Wakelock.disable();
+  // super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
